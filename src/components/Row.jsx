9a6ecdf9ef1292a61aa77b527c16596191103a74
Apurva Gupta <apurvagt1@gmail.com>
2023-07-30 13:50:59 +0530

    added hover effect on each movie card

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -16,11 +16,14 @@ const Row = ({ title, fetchUrl }) => {
       <div className="relative flex items-center">
         <div id={"slider"}>
           {movies.map((item, id) => (
-            <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer">
-              <img
+            <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
+              <img className="w-full h-auto block"
                 src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
                 alt="{item.title}"
               />
+              <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 hover:opacity-100 opacity-0 text-white ">
+                <p className="">{item?.title}</p>
+              </div>
             </div>
           ))}
         </div>
